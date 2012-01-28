1edb46ec6468012695a3bd4195c0f7dbd1b4bf1c
olim7t <omichallat+github@gmail.com>
2012-01-28 23:55:36 +0100

    Detail connection options (even though we're using the default ones)

--- demo/casbah.scala
+++ demo/casbah.scala
@@ -1,4 +1,8 @@
 import com.mongodb.casbah.Imports._
 
-val connection = MongoConnection()
-val db = connection("test")
+val mongoHost = "localhost"
+val mongoPort = 27017
+val mongoDbName = "test"
+
+val connection = MongoConnection(mongoHost, mongoPort)
+val db = connection(mongoDbName)
--- demo/lift.scala
+++ demo/lift.scala
@@ -1,9 +1,14 @@
-import com.mongodb.Mongo
+import com.mongodb.{Mongo, ServerAddress}
 import net.liftweb.mongodb.{DefaultMongoIdentifier, MongoDB}
 import com.foursquare.rogue.Rogue._
 import org.bson.types._
 import net.liftweb.json.JsonDSL._
 
-MongoDB.defineDb(DefaultMongoIdentifier, new Mongo, "test")
+val mongoHost = "localhost"
+val mongoPort = 27017
+val mongoDbName = "test"
+
+val mongoAddress = new ServerAddress(mongoHost, mongoPort)
+MongoDB.defineDb(DefaultMongoIdentifier, new Mongo(mongoAddress), mongoDbName)
 
 import examples.lift._
